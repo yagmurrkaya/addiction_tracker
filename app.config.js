dd2f86bd7cdcfea8b9f8a0d10910192c5d67f624
Yağmur KAYA <yagmurkaya1212@gmail.com>
2026-03-13 20:03:28 +0300

    apk  için düzenlemeler

---
+++ app.config.js
@@ -0,0 +1,68 @@
+export default {
+  expo: {
+    name: "addiction_tracker",
+    slug: "addiction_tracker",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "addictiontracker",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    extra: {
+      eas: {
+        projectId: "a50efa03-047e-4065-8955-340a2dfad967",
+      },
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.yagmur.addictiontracker",
+      infoPlist: {
+        NSLocationWhenInUseUsageDescription:
+          "Riskli bölgeleri haritada görebilmeniz için konum izni gereklidir.",
+        NSLocationAlwaysAndWhenInUseUsageDescription:
+          "Uygulama kapalıyken bile sizi koruyabilmemiz için konum iznini 'Her Zaman' yapmalısınız.",
+        NSLocationAlwaysUsageDescription:
+          "Arka planda konum takibi yaparak riskli bölgelerde bildirim gönderebilmemiz için bu izin şarttır.",
+        UIBackgroundModes: ["location", "fetch", "remote-notification"],
+      },
+    },
+    android: {
+      package: "com.yagmur.addictiontracker",
+      permissions: [
+        "ACCESS_COARSE_LOCATION",
+        "ACCESS_FINE_LOCATION",
+        "ACCESS_BACKGROUND_LOCATION",
+        "FOREGROUND_SERVICE",
+        "FOREGROUND_SERVICE_LOCATION",
+      ],
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+        },
+      },
+    },
+    plugins: [
+      "expo-router",
+      "expo-secure-store",
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission:
+            "Riskli bölgelerde sizi koruyabilmemiz için 'Her Zaman' konum iznine ihtiyacımız var.",
+          isAndroidBackgroundLocationEnabled: true,
+          isAndroidForegroundServiceEnabled: true,
+        },
+      ],
+      [
+        "expo-notifications",
+        {
+          color: "#007AFF",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+  },
+};
